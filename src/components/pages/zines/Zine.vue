e9156b872027e9dbb756326bfb0ae20c7ddaf506
Allie <alliecaton3@gmail.com>
2021-07-24 11:31:53 -0400

    formatting image url for display

--- src/components/pages/zines/Zine.vue
+++ src/components/pages/zines/Zine.vue
@@ -1,13 +1,13 @@
 <template>
-	<h4>{{ zine.title }}</h4>
-	<p>{{ zine.body }}</p>
-	<div>
-		<img :src="urlFor(zine.images[0]).url()" :alt="zine.title" />
-	</div>
-	<span>
+	<div class="cont">
+		<div class="thumb-image">
+			<img :src="formatUrl(zine.images[0].asset._ref)" :alt="zine.title" />
+		</div>
+		<h4>{{ zine.title }}</h4>
+		<p>{{ zine.body }}</p>
 		<a :href="zine.link" target="_">store link</a> |
-		<a :href="twitter">twitter</a></span
-	>
+		<a :href="twitter">twitter</a>
+	</div>
 </template>
 
 <script>
@@ -23,6 +23,29 @@ export default {
 			console.log(this.zine)
 			console.log(this.zine.name)
 		},
+
+		formatUrl(url) {
+			const array = url.split('-')
+			array.shift()
+			return `https://cdn.sanity.io/images/9k3xxnla/production/${array[0]}-${array[1]}.${array[2]}`
+		},
 	},
 }
 </script>
+
+<style scoped>
+.thumb-image {
+	width: 200px;
+	height: 200px;
+	overflow: hidden;
+}
+
+img {
+	width: 100%;
+}
+
+.cont {
+	padding: 10%;
+	text-align: center;
+}
+</style>
